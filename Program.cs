fef564b7245a905b9afd9d62603fa2a958ba442f
Your Name <you@example.com>
2023-04-13 21:06:04 +0300

    add file

--- Program.cs
+++ Program.cs
@@ -7,9 +7,13 @@
             Square square1 = new Square(5);
             Square square2 = new Square(2);
             Circle circle1 = new Circle(3);
+            Triangle triangle = new Triangle(3, 4);
 
             Console.WriteLine($"Area of the square: {square1.GetArea()}");
+            Console.WriteLine($"Area of the square: {square2.GetArea()}");
+
             Console.WriteLine($"Area of the circle {circle1.GetArea()}");
+            Console.WriteLine($"triagle area  {triangle.GetArea()}");
         }
     }
 }
---
+++ Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOpShapes
+{
+    internal class Triangle : Shape
+    {
+        private int height;
+        private int baseTriangle;
+        public Triangle(int h, int b)
+        {
+            this.height = h;
+            this.baseTriangle = b;
+        }
+        public override double GetArea()
+        {
+            return this.height * this.baseTriangle / 2;
+        }
+    }
+}
