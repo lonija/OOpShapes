d7c96a7e930fc3548f7551d11ab9668999b89fbe
Your Name <you@example.com>
2023-04-13 19:34:47 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OOpShapes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square1 = new Square(5);
+            Square square2 = new Square(2);
+
+            Console.WriteLine($"Area of the square: {square1.GetArea()}");
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace OOpShapes
+{
+    internal class Square : Shape
+    {
+        private int side;
+        public Square(int a)
+        {
+            this.side = a;
+        }
+        public override int GetArea()
+        {
+            return this.side * this.side;
+        }
+    }
+}
